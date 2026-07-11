bccc6b87c84eaf33aa13aade0a4d9aed4c4fa286
Madipdai <lamamadip96@gmail.com>
2026-07-11 09:44:33 +0545

    Add MongoDB connection

---
+++ apps/client/src/models/Career.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface ICareerAssessment extends Document {
+  title: string;
+  questions: any[];
+  createdAt: Date;
+}
+
+const CareerAssessmentSchema = new Schema<ICareerAssessment>({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  questions: {
+    type: [Schema.Types.Mixed],
+    required: true,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default model<ICareerAssessment>(
+  "CareerAssessment",
+  CareerAssessmentSchema
+);
